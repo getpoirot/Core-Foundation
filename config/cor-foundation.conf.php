a84c58f7f2781baf0d4b66822f7dc982d5f87097
Payam Naderi <naderi.payam@gmail.com>
2017-05-15 17:55:37 +0430

    code cleanup

--- config/cor-foundation.conf.php
+++ config/cor-foundation.conf.php
@@ -3,6 +3,20 @@ use Module\Foundation\ServiceManager\ServiceViewModelResolver;
 
 return [
     ServiceViewModelResolver::CONF_KEY => [
+        /*
+         * > Setup Aggregate Loader
+         *   Options:
+         *  [
+         *    'attach' => [new Loader(), $priority => new OtherLoader(), ['loader' => iLoader, 'priority' => $pr] ],
+         *    Loader::class => [
+         *       // Options
+         *       'Poirot\AaResponder'  => [APP_DIR_VENDOR.'/poirot/action-responder/Poirot/AaResponder'],
+         *       'Poirot\Application'  => [APP_DIR_VENDOR.'/poirot/application/Poirot/Application'],
+         *    ],
+         *    OtherLoader::class => [options..]
+         *  ]
+         */
+
         /** @see \Poirot\Loader\LoaderAggregate */
     ],
 ];
--- src/_ioc-facade.php
+++ src/_ioc-facade.php
@@ -2,20 +2,12 @@
 namespace Module\Foundation\Actions
 {
     use Module\Foundation\Actions\Helper\CycleAction;
-    use Module\Foundation\Actions\Helper\FlashMessageAction;
-    use Module\Foundation\Actions\Helper\HtmlLinkAction;
-    use Module\Foundation\Actions\Helper\HtmlScriptAction;
-    use Module\Foundation\Actions\Helper\UrlAction;
     use Module\Foundation\Actions\Helper\ViewAction;
     use Module\Foundation\Services\PathService\PathAction;
 
     /**
      * @method static ViewAction         view($template = null, $variables = null)
-     * @method static UrlAction          url($routeName = null, $params = array())
      * @method static PathAction         path($pathString = null, $variables = array())
-     * @method static FlashMessageAction flashMessage($messageNamespace = 'info')
-     * @method static HtmlScriptAction   htmlScript($section = 'inline')
-     * @method static HtmlLinkAction     htmlLink()
      * @method static CycleAction        cycle($action = null, $steps = 1, $reset = true)
      */
     class IOC extends \IOC
