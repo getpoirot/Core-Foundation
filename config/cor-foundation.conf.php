bfb621767cafe1df484980ff32a9ed67f0a8d112
Payam Naderi <naderi.payam@gmail.com>
2017-05-28 13:12:41 +0430

    viewModel resolver without merged config

--- config/cor-foundation.conf.php
+++ config/cor-foundation.conf.php
@@ -1,22 +1,5 @@
 <?php
-use Module\Foundation\ServiceManager\ServiceViewModelResolver;
 
 return [
-    ServiceViewModelResolver::CONF => [
-        /*
-         * > Setup Aggregate Loader
-         *   Options:
-         *  [
-         *    'attach' => [new Loader(), $priority => new OtherLoader(), ['loader' => iLoader, 'priority' => $pr] ],
-         *    Loader::class => [
-         *       // Options
-         *       'Poirot\AaResponder'  => [APP_DIR_VENDOR.'/poirot/action-responder/Poirot/AaResponder'],
-         *       'Poirot\Application'  => [APP_DIR_VENDOR.'/poirot/application/Poirot/Application'],
-         *    ],
-         *    OtherLoader::class => [options..]
-         *  ]
-         */
 
-        /** @see \Poirot\Loader\LoaderAggregate */
-    ],
 ];
--- src/ServiceManager/ServiceViewModelResolver.php
+++ src/ServiceManager/ServiceViewModelResolver.php
@@ -1,21 +1,16 @@
 <?php
 namespace Module\Foundation\ServiceManager;
 
-use Poirot\Application\aSapi;
 use Poirot\Ioc\Container\Service\aServiceContainer;
 
 use Poirot\Loader\LoaderAggregate;
 use Poirot\Loader\LoaderMapResource;
 use Poirot\Loader\LoaderNamespaceStack;
-use Poirot\Std\Struct\DataEntity;
 
 
 class ServiceViewModelResolver
     extends aServiceContainer
 {
-    const CONF = 'resolver';
-
-
     /**
      * @var string Service Name
      */
@@ -36,19 +31,6 @@ class ServiceViewModelResolver
                 return \Poirot\Loader\funcWatchFileExists($name, $resource, $match, '.'.$this->_layout_extension);
             }
         ));
-
-
-        # Setup By Configs:
-        $services = $this->services();
-
-        /** @var aSapi $config */
-        $config   = $services->get('/sapi');
-        $config   = $config->config();
-        /** @var DataEntity $config */
-        $config   = $config->get( self::CONF, [] );
-
-        if (! empty($config) )
-            $loader->with( $loader::parseWith($config), true );
 
 
         return $loader;
